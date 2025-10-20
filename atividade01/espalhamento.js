60a2cd4e6c4f6505e70ecc56886097934a350345
MoniqueeSilva <moniquedasilva2005@gmail.com>
2025-10-20 14:20:15 -0300

    espalhamento + desestruturação

--- atividade01/espalhamento.js
+++ atividade01/espalhamento.js
@@ -1,13 +1,7 @@
 const misto = ['pão', 'queijo', 'presunto'];
+const novaLista = [...misto, 'tomate', 'orégano']; // direto no spread
+const [primeiro, segundo, terceiro] = novaLista;
 
-function adicionarElemento(lista, ...novoElemento){ //rest (junta)
-    return [...lista, ...novoElemento]; //spread (espalha)
-}
-
-const novaLista = adicionarElemento (misto, 'tomate', 'orégano');
-
-const [primeiro, segundo, terceiro, quarto, quinto] = novaLista;
-
-console.log('primeira lista: ', misto);
-console.log('lista com novos elementos: ', novaLista);
-console.log(`itens desestruturados: ${primeiro}, ${segundo}, ${terceiro}, ${quarto}, ${quinto}`);
+console.log('primeira lista:', misto);
+console.log('lista com novos elementos:', novaLista);
+console.log(`itens desestruturados: ${primeiro}, ${segundo}, ${terceiro}`);
